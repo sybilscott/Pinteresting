24628137cff6de9e0e480fb5b24fa3d888a5724b
Sybil Scott <sybil.scott@houstontexans.comgit config --global user.email sybil.scott@houstontexans.comgit config --global user.email sybil.scott@houstontexans.com>
2014-02-12 16:17:29 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pinteresting::Application.config.secret_key_base = 'c31aa2261fc885acf4c10a7c036dd660ba6d0a2b6e8e86b04c18946b6edae6065b2a15be19f3b0659974406455df174c0c8ceeb7d61b382688ef717afd7702dd'
